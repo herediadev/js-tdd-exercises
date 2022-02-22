884a9084821030c4f05e1d44eb9acf8af6b9d03b
romina torres <rtorres@mobdev.cl>
2022-02-22 18:19:31 -0300

    ejercicio 3 - II.write-tests

--- II.write-tests/03-remove-vowels-from-array/remove-vowels-in-array.js
+++ II.write-tests/03-remove-vowels-from-array/remove-vowels-in-array.js
@@ -1,9 +1,9 @@
-const removeVowelsForWords = (words) => {
-  let result = [];
-  const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+const {removeVowels} = require("../02-remove-vowels/remove-vowels");
 
+const removeVowelsForWords = (words) => {
+  
   const result = words.map((word) => {
-    return removeVowelsForWords(word);
+    return removeVowels(word);
   });
 
   return result;
--- II.write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
+++ II.write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
@@ -3,11 +3,11 @@ const {removeVowelsForWords} = require("./remove-vowels-in-array");
 describe("remove vowels from all words in array", () => {
   it("For the ['Irina', 'Etza', 'Daniel'] it will return ['rn', 'tz', 'Dnl']", () => {
     // Arrange
-    const word = ['Irina', 'Etza', 'Daniel'];
-    const expected = ['rn', 'tz', 'Dnl'];
+    const input = ['Irina', 'Etza', 'Daniel'];
+    const expected = ['_r_n_', '_tz_', 'D_n__l'];
     
     // Act
-    const result = removeVowelsForWords(word);
+    const result = removeVowelsForWords(input);
 
     // Assert
     expect(result).toEqual(expected);
