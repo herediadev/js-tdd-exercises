664096cb6b18a43e6ed3eee081f0eefcf512e921
romina torres <rtorres@mobdev.cl>
2022-02-22 20:11:28 -0300

    ejercicio 6 - II.write-tests

--- II.write-tests/06-largest-number/largest-number.js
+++ II.write-tests/06-largest-number/largest-number.js
@@ -1,11 +1,24 @@
-function getLargestNumber(array) {
-  var largestNumber;
-  for (var i = 0; i < array.length - 1; i++) {
+const getLargestNumber = (array) => {
+  if (!Array.isArray(array)) {
+    throw new Error("Error");
+  }
+  if(array.length<2){
+    throw new Error("Error");
+  }
+  
+  const largestNumber = array.sort((a, b) => b - a)
+  return largestNumber[0]
+}
+  /*
+  let largestNumber;
+  for (let i = 0; i < array.length - 1; i++) {
     if (array[i] > array[i + 1]) {
       largestNumber = array[i];
     }
   }
   return largestNumber;
-}
+}*/
 
-module.exports = getLargestNumber;
+module.exports = {
+  getLargestNumber}
+  ;
