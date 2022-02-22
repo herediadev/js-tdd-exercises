84364e5d9330e35ba274af89fceb82ea35ce35a8
romina torres <rtorres@mobdev.cl>
2022-02-22 18:36:30 -0300

    ejercicio 5 - II.write-tests

--- II.write-tests/05-get-second-third-smallest/get-second-third.js
+++ II.write-tests/05-get-second-third-smallest/get-second-third.js
@@ -1,7 +1,11 @@
-module.exports = function(array) {
+const getSecondThird = (array) => {
   const newArray = array.slice();
-  newArray.sort(function(x, y) {
+  newArray.sort((x, y) => {
     return x - y;
   });
   return [newArray[1], newArray[2]];
 };
+
+module.exports = {
+  getSecondThird
+}
