eac01c70fd303a145dc5ca8178f054829dc49281
romina torres <rtorres@mobdev.cl>
2022-03-07 18:06:39 -0300

    agregando tests y refactorizando

--- III.tdd-katas/fizz-buzz/fizz-buzz.js
+++ III.tdd-katas/fizz-buzz/fizz-buzz.js
@@ -4,19 +4,23 @@ const fizzbuzz = (number) => {
         arr.push(i)
     }
 
-    const arrayOrder = arr.sort()
+    const arrayOrder = arr.sort((a, b) => {
+        return a - b
+    })
+    console.log(arrayOrder)
     
     const arrayNumbers = arrayOrder.map(number => {
         if(number % 3 === 0 && number % 5 === 0){
             return "FizzBuzz"
-        }if(number % 3 === 0){
+        }if(number % 3 === 0 || number.toString().includes('3')){
             return "Fizz"
-        }if(number % 5 === 0){
+        }if(number % 5 === 0 || number.toString().includes('5')){
             return "Buzz"
         }else{
             return number
         }
     })
+    console.log(arrayNumbers)
 
     return arrayNumbers.join(', ');
 
--- III.tdd-katas/fizz-buzz/fizz-buzz.test.js
+++ III.tdd-katas/fizz-buzz/fizz-buzz.test.js
@@ -1,7 +1,7 @@
 const { fizzbuzz } = require("./fizz-buzz");
 
-describe("prints 1", () => {
-  it("prints 1", () => {
+describe("Fizzbuzz function", () => {
+  it("For the 1 it will return '1' ", () => {
     //Arrange
     const number = 1;
     const expected = "1";
@@ -14,10 +14,69 @@ describe("prints 1", () => {
     expect(result).toEqual(expected);
   });
 
-  it("prints 1,2", () => {
+  it("For the 2 it will return '1,2' ", () => {
     //Arrange
     const number = 2;
     const expected = "1, 2";
+
+    //Act
+    const result = fizzbuzz(number);
+
+    //Assert
+    expect(result).toEqual(expected);
+  });
+
+  it("For the 3 it will return '1, 2, 3' ", () => {
+    //Arrange
+    const number = 3;
+    const expected = "1, 2, Fizz";
+
+    //Act
+    const result = fizzbuzz(number);
+
+    //Assert
+    expect(result).toEqual(expected);
+  });
+  it("For the 5 it will return '1, 2, 3, 4, 5' ", () => {
+    //Arrange
+    const number = 5;
+    const expected = "1, 2, Fizz, 4, Buzz";
+
+    //Act
+    const result = fizzbuzz(number);
+
+    //Assert
+    expect(result).toEqual(expected);
+  });
+
+  it("For the 10 it will return '1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz' ", () => {
+    //Arrange
+    const number = 10;
+    const expected = "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz";
+
+    //Act
+    const result = fizzbuzz(number);
+
+    //Assert
+    expect(result).toEqual(expected);
+  });
+
+  it("For the 20 it will return '1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, Fizz, 14, FizzBuzz, 16, 17, Fizz, 19, Buzz' ", () => {
+    //Arrange
+    const number = 20;
+    const expected = "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, Fizz, 14, FizzBuzz, 16, 17, Fizz, 19, Buzz";
+
+    //Act
+    const result = fizzbuzz(number);
+
+    //Assert
+    expect(result).toEqual(expected);
+  });
+
+  it("For the 50 it will return '1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, Fizz, 14, FizzBuzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, Fizz, Fizz, Buzz, 26, Fizz, 28, 29, FizzBuzz, Fizz, Fizz, Fizz, Fizz, Fizz, Fizz, Fizz, Fizz, Fizz, Buzz, 41, Fizz, Fizz, 44, FizzBuzz, 46, 47, Fizz, 49, Buzz' ", () => {
+    //Arrange
+    const number = 50;
+    const expected = "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, Fizz, 14, FizzBuzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, Fizz, Fizz, Buzz, 26, Fizz, 28, 29, FizzBuzz, Fizz, Fizz, Fizz, Fizz, Fizz, Fizz, Fizz, Fizz, Fizz, Buzz, 41, Fizz, Fizz, 44, FizzBuzz, 46, 47, Fizz, 49, Buzz";
 
     //Act
     const result = fizzbuzz(number);
