8076df6e13083a7042b84aa20956925908c37989
romina torres <rtorres@mobdev.cl>
2022-03-09 15:52:10 -0300

    add the following verifications

--- III.tdd-katas/password-verifier/password-verifier.js
+++ III.tdd-katas/password-verifier/password-verifier.js
@@ -4,12 +4,41 @@
     }
 
     isLengthGratherThan(limit){
-        const message = "La password no puede contener menos de 8 caracteres";
+        const message = "The password cannot contain less than 8 characters";
         if(this.password.length < limit){
             throw new Error(message)
         }
        
     }
+
+    isNotNull(){
+        if(this.password === null){
+            throw new Error("The password cannot be null")
+        }
+        return true;
+    }
+
+    isNotContainToUppercase(){
+        if(this.password.match(/[A-Z]/g) === null){
+            throw new Error("The password should be contain at least one uppercase")
+        }
+    }
+
+    isNotContainToLowercase(){
+        if(this.password.match(/[a-z]/g) === null){
+            throw new Error("The password should be contain at least one lowercase")
+        }
+    }
+
+    isNotContainNumber(){
+        if(this.password.match(/\d+/g) === null){
+            throw new Error("The password should be contain at least one number")
+        }
+    }
+
+    
+
+      
   }
 
   module.exports = PasswordVerifier;
--- III.tdd-katas/password-verifier/password-verifier.test.js
+++ III.tdd-katas/password-verifier/password-verifier.test.js
@@ -1,17 +1,57 @@
 const PasswordVerifier = require("./password-verifier");
 
 describe("PasswordVerifier function", () => {
-    it("should exist", () => {
+    it("Should exist", () => {
+        //Arrange
         const passwordVerifier = new PasswordVerifier("1234");
+
+        //Act
         const isInstanceOfPasswordVerifier = passwordVerifier instanceof PasswordVerifier;
+
+        //Assert
         expect(isInstanceOfPasswordVerifier).toBe(true);
     });
 
-    it("password is grather than 8 characters", () => {
+    it("Password is grather than 8 characters", () => {
+        //Arrange
         const passwordVerifier = new PasswordVerifier("1234");
-        expect(() => passwordVerifier.isLengthGratherThan(8)).toThrow("La password no puede contener menos de 8 caracteres");
+
+        //Act and assert
+        expect(() => passwordVerifier.isLengthGratherThan(8)).toThrow("The password cannot contain less than 8 characters");
     });
 
+    it("Password should not be null", () => {
+        //Arrange
+        const passwordVerifier = new PasswordVerifier(null);
 
+        //Act and assert
+        expect(() => passwordVerifier.isNotNull()).toThrow("The password cannot be null");
+    });
+
+    it("Password should be contain at least one uppercase", () => {
+        //Arrange
+        const passwordVerifier = new PasswordVerifier("123hola");
+
+        //Act and assert
+        expect(() => passwordVerifier.isNotContainToUppercase()).toThrow("The password should be contain at least one uppercase");
+    });
+
+    it("Password should be contain at least one lowercase", () => {
+        //Arrange
+        const passwordVerifier = new PasswordVerifier("123HOLA");
+
+        //Act and assert
+        expect(() => passwordVerifier.isNotContainToLowercase()).toThrow("The password should be contain at least one lowercase");
+    });
+
+    it("Password should be contain at least one number", () => {
+        //Arrange
+        const passwordVerifier = new PasswordVerifier("holaHOLA");
+
+        //Act and assert
+        expect(() => passwordVerifier.isNotContainNumber()).toThrow("The password should be contain at least one number");
+    });
+
+    
 
 });
