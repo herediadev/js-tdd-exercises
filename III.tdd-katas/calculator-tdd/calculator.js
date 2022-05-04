a6da46375c380c00c865c237d7ad909d26cb2ba3
Marcelo <mhuenchupan@mobdev.cl>
2022-05-04 16:09:52 -0400

    Avances calculator II

--- III.tdd-katas/calculator-tdd/calculator.js
+++ III.tdd-katas/calculator-tdd/calculator.js
@@ -1,13 +1,22 @@
+//??  function Calculator () {
+//   this.add = Add;
+// }
 
-function Add (numbers, delimiter = ',') {
+const Add = (numbers, delimiter = ',') => {
+  // Sanitize input
   const replace = numbers.replace(/\n/g, delimiter);
   const splitNumbers = replace.split(delimiter);
   const parseIntNumbers = splitNumbers.map(number => parseInt(number));
-  const filterNumbers = parseIntNumbers.filter(number => number < 1000);
-  const sum = filterNumbers.reduce((acc, number) => acc + number, 0);
-  const detectNegatives = filterNumbers.filter(number => number < 0);
-  
+  // End of sanitize
+
+  // Validate input
+  const ignoreMoreThan1000 = parseIntNumbers.filter(number => number <= 1000);
+  const detectNegatives = ignoreMoreThan1000.filter(number => number < 0);
   if (detectNegatives.length > 0) return `negatives not allowed: ${detectNegatives.join(',')}`;
+  // End of validate
+
+  // Calculates
+  const sum = ignoreMoreThan1000.reduce((acc, number) => acc + number, 0);
   return sum;
 }
 
--- III.tdd-katas/calculator-tdd/calculator.test.js
+++ III.tdd-katas/calculator-tdd/calculator.test.js
@@ -1,29 +1,35 @@
 const { Add } = require('./calculator');
 
-describe('calculator', () => {
-  it('part1', () => {
+describe('Add function', () => {
+  it('The string argument can contain 0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0) for example ``""`` or ``"1"`` or ``"1,2"``.', () => {
 	expect(Add('0')).toBe(0);
 	expect(Add('1')).toBe(1);
 	expect(Add('1,2')).toBe(3);
   });
 
-  it('part2', () => {
+  it('Allow the ``add()`` method to handle an unknown amount of numbers.', () => {
 	expect(Add('1,2,3')).toBe(6);
 	expect(Add('1,2,3,4')).toBe(10);
 	expect(Add('1,2,3,4,5')).toBe(15);
   })
 
-  it('part3', () => {
+  it('Allow the ``add()`` method to handle new lines between numbers (instead of commas). * the following input is ok:  ``"1\n2,3"`` (will equal 6)', () => {
 	expect(Add('1\n2,3')).toBe(6);
 	expect(Add('1\n2\n3\n4,5')).toBe(15);
   })
 
-  it('test of delimitiers', () => {
+  it('Support different delimiters: to change a delimiter?', () => {
 	expect(Add("//;\n1;2", ';')).toBe(3);
+	expect(Add("//!2!3!4", '!')).toBe(9);
   })
 
-  it('test of negative numbers', () => {
+  it('The existence of negative numbers must be verified, in case of containing it, throw a message ...', () => {
 	expect(Add('-1,2')).toBe('negatives not allowed: -1');
 	expect(Add('-1,-2,3')).toBe('negatives not allowed: -1,-2');
+  })
+
+  it('Should be verified that numbers greater than 1000 should be ignored.', () => {
+	expect(Add('1001')).toBe(0);
+	expect(Add('1001,2,3,4,5')).toBe(14);
   })
 })
