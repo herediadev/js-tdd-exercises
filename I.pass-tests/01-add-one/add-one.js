9e376d5add466642d0eb01a6e44c98609c94cad5
FabiSantos <fabianesantos.developer@gmail.com>
2022-07-27 12:05:57 -0400

    refactor add-one

--- I.pass-tests/01-add-one/add-one.js
+++ I.pass-tests/01-add-one/add-one.js
@@ -1,3 +1,32 @@
-const addOne = (numbers) =>  numbers.map(number => number+1);
+const addOne = (numbers) => {
+  if (!Array.isArray(numbers)) {
+    throw new Error("¡Ocurrió un error!")
+  }
+  const isNumber = numbers.every(num => typeof num === "number");
+  if (!isNumber) {
+    throw new Error("¡Ocurrió un error!");
+  }
+  return numbers.map(number => ++number);
+}
 
-module.exports = {addOne};
+module.exports = { addOne };
+
+//   if (numbers) {
+//     const isNumber = numbers.every(num => typeof num === "number");
+//     if (isNumber) {
+//       return numbers.map(number => ++number);
+//     } else {
+//       throw new Error("¡Ocurrió un error!");
+//     }
+//   } else {
+//     throw new Error("¡Ocurrió un error!");
+//   }
+// }
+  // if (!Array.isArray(numbers)) {
+  //   throw new Error("¡Ocurrió un error!")
+  // }
+  // const isNumber = numbers.every(num => typeof num === "number");
+  // if (isNumber) {
+  //   return numbers.map(number => ++number);
+  // }
+
--- I.pass-tests/01-add-one/add-one.test.js
+++ I.pass-tests/01-add-one/add-one.test.js
@@ -1,15 +1,63 @@
-const {addOne} = require("./add-one.js");
+const { addOne } = require("./add-one.js");
 describe("add-one function", () => {
-  const myArray = [31, 57, 12, 5];
-  const unchanged = [31, 57, 12, 5];
-  const expected = [32, 58, 13, 6];
-    it("should return a new array and add 1 to each item",() => {
-      const output = addOne(myArray);
+  it("given the input [31, 57, 12, 5], adding 1 to each item should return [32, 58, 13, 6]", () => {
+    //arrange
+    const myArray = [31, 57, 12, 5];
+    const expected = [32, 58, 13, 6];
+    //act
+    const output = addOne(myArray);
+    //assert
     expect(output).toEqual(expected);
-    });
-    it("should not change myArray value",() => {
-      addOne(myArray);
-      expect(myArray).toEqual(unchanged);
-    });
+  });
+  it("given the input [31, 57, 12, 5], should not change the input value", () => {
+    //arrange
+    const myArray = [31, 57, 12, 5];
+    const unchanged = [31, 57, 12, 5];
+    //act
+    addOne(myArray);
+    //assert
+    expect(myArray).toEqual(unchanged);
+  });
+
+  it("given an empty input(undefined), should throw an error", () => {
+    //arrange
+    const input = undefined;
+    const output = () => addOne(input);
+    //act and assert
+    expect(output).toThrow(Error);
+  });
+
+  it("given the input null, should throw an error", () => {
+    //arrange
+    const input = null;
+    const output = () => addOne(input);
+    //act and assert
+    expect(output).toThrow(Error);
+  });
+
+  it("given the input string 'hola', should throw an error", () => {
+    //arrange
+    const input = "hola";
+    const output = () => addOne(input);
+    //act and assert
+    expect(output).toThrow(Error);
+  });
+
+  it("given the input [], should return []", () => {
+    //arrange
+    const input = [];
+    //act
+    const output = addOne(input);
+    //assert
+    expect(output).toEqual(input);
+  });
+
+  it("given the input [true, true, true], should throw an error", () => {
+    //arrange
+    const input = [true, true, true];
+    const output = () => addOne(input);
+    //act and assert
+    expect(output).toThrow(Error);
+  });
 })
 
