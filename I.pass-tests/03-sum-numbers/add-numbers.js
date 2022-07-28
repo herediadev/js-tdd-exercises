4f89a155c07f2d6a49f95b7d69c7317d1d7a2f8d
FabiSantos <fabianesantos.developer@gmail.com>
2022-07-27 22:10:44 -0400

    refactor add-numbers

--- I.pass-tests/03-sum-numbers/add-numbers.js
+++ I.pass-tests/03-sum-numbers/add-numbers.js
@@ -1,5 +1,12 @@
 const addNumbers = (numbers) => {
-    return numbers.reduce((a, b) => a + b)
-};  
+    if (!Array.isArray(numbers)) {
+        throw new Error("¡Ocurrió un error!");
+    }
+    const isNumber = numbers.every(number => typeof number === "number");
+    if (!isNumber) {
+        throw new Error("¡Ocurrió un error!");
+    }
+    return numbers.reduce((a, b) => a + b);
+};
 
-module.exports = {addNumbers};
+module.exports = { addNumbers };
--- I.pass-tests/03-sum-numbers/add-numbers.test.js
+++ I.pass-tests/03-sum-numbers/add-numbers.test.js
@@ -1,9 +1,28 @@
-const {addNumbers} = require("./add-numbers");
+const { addNumbers } = require("./add-numbers");
 describe("add-numbers function", () => {
-  it("add all the numbers in the array", () => {
+  it("given the input [9, 23, 10, 3, 8], should return the sum 53", () => {
+    //arrange
     const numbers = [9, 23, 10, 3, 8];
     const expected = 53;
+    //act
     const output = addNumbers(numbers);
-  expect(output).toEqual(expected);
+    //assert
+    expect(output).toEqual(expected);
+  });
+
+  it("given the input 'helo word', should throw an error", () => {
+    //arrange
+    const numbers = "helo word";
+    const output = () => addNumbers(numbers);
+    //act and assert
+    expect(output).toThrow(Error);
+  });
+
+  it("given the input [true, true, true], should throw an error", () => {
+    //arrange
+    const numbers = [true, true, true];
+    const output = () => addNumbers(numbers);
+    //act and assert
+    expect(output).toThrow(Error);
   });
 });
