e16f5a4f26f442a7e9005259a1f012b410cf1080
FabiSantos <fabianesantos.developer@gmail.com>
2022-07-27 21:19:43 -0400

    refactor get-word-lengths

--- I.pass-tests/02-get-word-lengths/get-word-lengths.js
+++ I.pass-tests/02-get-word-lengths/get-word-lengths.js
@@ -1,2 +1,17 @@
-module.exports = getWordLengths = (someWords) => someWords.map(word => word.length);
+const getWordLengths = (someWords) => {
+  if (!Array.isArray(someWords)) {
+    throw new Error("¡Ocurrió un error!");
+  }
+  const isString = someWords.every(word => typeof word === "string");
+  if (!isString) {
+    throw new Error("¡Ocurrió un error!");
+  }
+  return someWords.map(word => word.length);
+  // if (Array.isArray(someWords)) {
+  //   return someWords.map(word => word.length);
+  // }else{
+  //   throw new Error("¡Ocurrió un error!");
+  // }
+}
 
+module.exports = { getWordLengths };
--- I.pass-tests/02-get-word-lengths/get-word-lengths.test.js
+++ I.pass-tests/02-get-word-lengths/get-word-lengths.test.js
@@ -1,9 +1,29 @@
-const wordLengths = require("./get-word-lengths");
+const { getWordLengths } = require("./get-word-lengths");
 describe("get-word-lengths function", () => {
-  it("should return a new array with the number of characters in each word", () => {
+  it("should return a new array [3, 6, 10, 5], with the number of characters in each word", () => {
+    //arrange
     const words = ["sun", "potato", "roundabout", "pizza"];
     const expected = [3, 6, 10, 5];
-    const output = wordLengths(words);
-  expect(output).toEqual(expected);
+    //act
+    const output = getWordLengths(words);
+    //assert
+    expect(output).toEqual(expected);
+  });
+
+  it("given the input different of type array, should throw an error", () => {
+    //arrange
+    const words = "sun";
+    //act
+    const output = () => getWordLengths(words);
+    //assert
+    expect(output).toThrow(Error);
+  });
+
+  it("given the input [true, 2, null], should throw an error", () => {
+    //arrange
+    const input = [true, 2, null];
+    const output = () => addOne(input);
+    //act and assert
+    expect(output).toThrow(Error);
   });
 });
