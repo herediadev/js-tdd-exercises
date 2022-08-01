49e6c466ab6d94d73bb2727f086f9a8431d04732
FabiSantos <fabianesantos.developer@gmail.com>
2022-08-01 11:11:45 -0400

    soluction for exercise get- average

--- I.pass-tests/08-get-average/get-average.js
+++ I.pass-tests/08-get-average/get-average.js
@@ -1,3 +1,21 @@
 // the input is an array of numbers and strings
 // return the average of all the numbers
 // be sure to exclude the strings
+// a entrada é um array de números e strings
+// retorna a média de todos os números
+// certifique-se de excluir as strings
+
+const average = (numbersAndStrings) => {
+    if (!Array.isArray) {
+        throw new Error("¡Ocurrió un error!");
+    }
+    const isNumber = numbersAndStrings.every(num => typeof num !== "number" || typeof num !== "string");
+    if (!isNumber) {
+        throw new Error("¡Ocurrió un error!");
+    }
+    const justNumbers = numbersAndStrings.filter(num => typeof num === "number");
+    const numberLength = justNumbers.length;
+    return justNumbers.reduce((a, b) => a + b) / numberLength;
+}
+
+module.exports = { average };
--- I.pass-tests/08-get-average/get-average.test.js
+++ I.pass-tests/08-get-average/get-average.test.js
@@ -1,10 +1,51 @@
-var average = require("./get-average");
+const { average } = require("./get-average");
 
-test("Average", function() {
-  var numbers = [4, "-", 8, 11, "hello", "57", 0, 2];
-  var expected = 5;
+describe("get-Average function", () => {
+  it("Given the array [4, " - ", 8, 11, 'hello', '57', 0, 2], should return average of all numbers", () => {
+    //arrange
+    const numbers = [4, "-", 8, 11, "hello", "57", 0, 2];
+    const expected = 5;
+    //act
+    const output = average(numbers);
+    //assert
+    expect(output).toEqual(expected);
+  });
 
-  var output = average(numbers);
+  it('given the array [4, "-", 8, 11, "hello", "57", 0, 2], should not return [4, 8, 11, 0, 2]', () => {
+    //arrange
+    const numbers = [4, "-", 8, 11, "hello", "57", 0, 2];
+    const isNotAcepted = [4, 8, 11, 0, 2];
+    //act
+    const output = average(numbers);
+    //assert
+    expect(output).not.toEqual(isNotAcepted);
+  });
 
-  expect(output).toEqual(expected);
+  it("given the input('hola'), should  throw an error", () => {
+    //arrange
+    const input = "hola";
+    const output = () => average(input);
+    //act and assert
+    expect(output).toThrow(Error);
+  });
+
+  it("given an empty input(undefined), should throw an error", () => {
+    //arrange
+    const input = undefined;
+    const output = () => average(input);
+    //act and assert
+    expect(output).toThrow(Error);
+  });
+
+  it("given the input null, should throw an error", () => {
+    //arrange
+    const input = null;
+    const output = () => average(input);
+    //act and assert
+    expect(output).toThrow(Error);
+  });
+
 });
+
+
+// metodo every para seleccionar solo strings? lenght del array, reduce del array divido por el length
