81c85682c7db90de3d2eebb0987549399570efff
FabiSantos <fabianesantos.developer@gmail.com>
2022-08-02 10:00:26 -0400

    soluction for exercise factorial

--- I.pass-tests/05-factorial/factorial.js
+++ I.pass-tests/05-factorial/factorial.js
@@ -1,13 +1,16 @@
-// int is an integer
-// a factorial is the product of all non-negative integers
-// less than or equal to the iniital number.
+const factorial = (int) => {
+    if (int < 0) {
+        throw new Error("¡Ocurrió un error!")
+    }
+    const isNumber = typeof int === "number";
+    if (!isNumber) {
+        throw new Error("¡Ocurrió un error!");
+    }
+    let total = 1;
+    for (let i = int; i >= 1; i--) {
+        total = total * i;
+    }
+    return total;
+}
 
-// for example the factorial of 5 is 120
-// 120 = 1 * 2 * 3 * 4 * 5
-
-// calculate and return the factorial of int
-// note: factorial of 0 is 1
-
-function factorial(int) {}
-
-module.exports = factorial;
+module.exports = { factorial };
--- I.pass-tests/05-factorial/factorial.test.js
+++ I.pass-tests/05-factorial/factorial.test.js
@@ -1,32 +1,64 @@
-var factorial = require("./factorial");
+const { factorial } = require("./factorial");
 
-describe("Factorial", function() {
-  test("Factorial", function() {
-    var in1 = 5;
-    var exp1 = 120;
+describe("Factorial function", () => {
+  it("Given the input, example: the number 5, should return the number 120", () => {
+    const in1 = 5;
+    const exp1 = 120;
 
-    var in2 = 9;
-    var exp2 = 362880;
+    const in2 = 9;
+    const exp2 = 362880;
 
-    var in3 = 1;
-    var exp3 = 1;
+    const in3 = 1;
+    const exp3 = 1;
 
-    var in4 = 0;
-    var exp4 = 1;
+    const in4 = 0;
+    const exp4 = 1;
 
-    var in5 = 3;
-    var exp5 = 6;
+    const in5 = 3;
+    const exp5 = 6;
 
-    var out1 = factorial(in1);
-    var out2 = factorial(in2);
-    var out3 = factorial(in3);
-    var out4 = factorial(in4);
-    var out5 = factorial(in5);
+    const out1 = factorial(in1);
+    const out2 = factorial(in2);
+    const out3 = factorial(in3);
+    const out4 = factorial(in4);
+    const out5 = factorial(in5);
 
     expect(out1).toEqual(exp1);
     expect(out2).toEqual(exp2);
     expect(out3).toEqual(exp3);
     expect(out4).toEqual(exp4);
     expect(out5).toEqual(exp5);
+  });
+
+  it("Given the input -2, should throw an error", () => {
+    //arrange
+    const int = -2;
+    const output = () => factorial(int);
+    //act and assert
+    expect(output).toThrow(Error);
+  });
+
+  it("given the input null, should throw an error", () => {
+    //arrange
+    const input = null;
+    const output = () => addOne(input);
+    //act and assert
+    expect(output).toThrow(Error);
+  });
+
+  it("given the input string 'hola', should throw an error", () => {
+    //arrange
+    const input = "hola";
+    const output = () => addOne(input);
+    //act and assert
+    expect(output).toThrow(Error);
+  });
+
+  it("given the input [true, true, true], should throw an error", () => {
+    //arrange
+    const input = [true, true, true];
+    const output = () => addOne(input);
+    //act and assert
+    expect(output).toThrow(Error);
   });
 });
